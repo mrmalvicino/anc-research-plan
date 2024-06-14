b89083b8dfd75568117d2275d38e772a015a5b6d
mrmalvicino <mrmalvicino@gmail.com>
2024-06-14 05:14:06 -0300

    Inconsistent outliers

--- Statistics/functions.py
+++ Statistics/functions.py
@@ -28,21 +28,56 @@ def measured_noise(background_noise_mean, background_noise_deviation, delta_spl,
 
     return measured_noise
 
-def perceived_noise(samples_quantity, measurements_per_sample):
+def perceived_noise(samples_quantity, measurements_per_sample, anc_status, measured_noise, background_noise_max):
     perceived_noise = np.empty((samples_quantity, measurements_per_sample))
+    values = [1, 2]
 
     for i in range(samples_quantity):
         for j in range(measurements_per_sample):
-            perceived_noise[i][j] = rnd.randint(1, 2)
+            if (anc_status[i][j] == 1 or measured_noise[i][j] <= background_noise_max):
+                weights = [0.8, 0.2] # Si la ANC está encendida o se mide poco ruido, es más probable que escuche con poco ruido
+            else:
+                weights = [0.1, 0.9] # Si la ANC está apagada y se mide mucho ruido, es más probable que escuche con mucho ruido
+
+            perceived_noise[i][j] = rnd.choices(values, weights)[0]
 
     return perceived_noise
 
 
-def perceived_quality(samples_quantity, measurements_per_sample):
+def perceived_quality(samples_quantity, measurements_per_sample, anc_status):
     perceived_quality = np.empty((samples_quantity, measurements_per_sample))
+    values = [1, 2]
 
     for i in range(samples_quantity):
         for j in range(measurements_per_sample):
-            perceived_quality[i][j] = rnd.randint(1, 2)
+            if (anc_status[i][j] == 1):
+                weights = [0.7, 0.3] # Si la ANC está encendida, es más probable que escuche con baja calidad
+            else:
+                weights = [0.3, 0.7] # Si la ANC está apagada, es más probable que escuche con alta calidad
+
+            perceived_quality[i][j] = rnd.choices(values, weights)[0]
 
     return perceived_quality
+
+def inconsistent_outliers(anc_status, measured_noise, perceived_noise, background_noise_max, samples_quantity, measurements_per_sample):
+    strike_1 = [] # Sensibles
+    strike_2 = [] # Sordos
+    outliers = [] # Sensibles y sordos (absurdo)
+
+    for i in range(samples_quantity):
+        for j in range(measurements_per_sample):
+            if (perceived_noise[i][j] == 2 and measured_noise[i][j] <= background_noise_max and anc_status[i][j] == 0): # Si percibe mucho ruido y solo hay ruido de fondo y la anc está apagada
+                if (i not in outliers): # No agregar dos veces
+                    strike_1.append(i) # Potencial outlier
+
+    for i in range(samples_quantity):
+        for j in range(measurements_per_sample):
+            if (perceived_noise[i][j] == 1 and background_noise_max < measured_noise[i][j] and anc_status[i][j] == 0): # Si percibe poco ruido cuando hay ruido generado y la anc está apagada
+                if (i not in outliers): # No agregar dos veces
+                    strike_2.append(i) # Potencial outlier
+
+    for i in range(samples_quantity):
+        if (i in strike_1 and i in strike_2):
+            outliers.append(i) # Es outlier
+
+    return outliers
